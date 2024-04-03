f7da17b27337475b6471e594a3eec1e96872125f
Pascal Paulis <ppaulis@gmail.com>
2024-04-03 10:13:04 +0200

    bugfixes
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- src/ElasticSearchPaginator/ElasticSearchMapperInterface.php
+++ src/ElasticSearchPaginator/ElasticSearchMapperInterface.php
@@ -6,12 +6,12 @@ interface ElasticSearchMapperInterface
 {
     /**
      * @param array $query
-     * @param int $offset
-     * @param int $countPerPage
+     * @param int $size
+     * @param int $page
      * @param array $sort
      * @return ElasticSearchEntityInterface[]
      */
-    public function searchIndex(array $query, int $offset, int $countPerPage, array $sort): array;
+    public function searchIndex(array $query, int $size = 5000, int $page = 1, array $sort = []): array;
 
     /**
      * @param array $query

be7660db467231f1eceda5430c2c25b6efa78157
Pascal Paulis <ppaulis@gmail.com>
2024-04-03 10:54:37 +0200

    working on interface
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- src/ElasticSearchPaginator/ElasticSearchMapperInterface.php
+++ src/ElasticSearchPaginator/ElasticSearchMapperInterface.php
@@ -5,6 +5,8 @@ namespace BayWaReLusy\ElasticSearchPaginator;
 interface ElasticSearchMapperInterface
 {
     /**
+     * Search the index with the given query.
+     *
      * @param array $query
      * @param int $size
      * @param int $page
@@ -14,8 +16,17 @@ interface ElasticSearchMapperInterface
     public function searchIndex(array $query, int $size = 5000, int $page = 1, array $sort = []): array;
 
     /**
+     * Get the number of results for the given query.
+     *
      * @param array $query
      * @return int
      */
     public function resultCount(array $query): int;
+
+    /**
+     * Get the name of the ElasticSearch index.
+     *
+     * @return string
+     */
+    public function getIndexName(): string;
 }
